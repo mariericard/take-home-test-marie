a74c6e073ca9754567e6a1bbce991d245f8260fb
mariericard <marie.ricard@mines-ales.org>
2023-05-26 13:33:14 +0200

    refactoring

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ import { Store, DiscountOffer } from "./store";
 import fs from "fs";
 
 const discountOffers = [
-  new DiscountOffer("Velib", 20, 30),
-  new DiscountOffer("Naturalia", 10, 5),
-  new DiscountOffer("Vinted", 5, 40),
-  new DiscountOffer("Ilek", 15, 40)
+    new DiscountOffer("Velib", 20, 30),
+    new DiscountOffer("Naturalia", 10, 5),
+    new DiscountOffer("Vinted", 5, 40),
+    new DiscountOffer("Ilek", 15, 40),
+    new DiscountOffer("BackMarket", 16, 40)
 ];
 const store = new Store(discountOffers);
 
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(store.updateDiscounts()));
+    log.push(JSON.stringify(store.updateDiscounts()));
 }
 
 /* eslint-disable no-console */
 fs.writeFile("output.txt", log, err => {
-  if (err) {
-    console.log("error");
-  } else {
-    console.log("success");
-  }
+    if (err) {
+        console.log("error");
+    } else {
+        console.log("success");
+    }
 });
 /* eslint-enable no-console */
--- store.js
+++ store.js
@@ -1,66 +1,127 @@
 export class DiscountOffer {
-  constructor(partnerName, expiresIn, discountRateInPercent) {
-    this.partnerName = partnerName;
-    this.expiresIn = expiresIn;
-    this.discountInPercent = discountRateInPercent;
-  }
+    constructor(partnerName, expiresIn, discountRateInPercent) {
+        this.partnerName = partnerName;
+        this.expiresIn = expiresIn;
+        this.discountInPercent = discountRateInPercent;
+    }
 }
 
-export class Store {
-  constructor(discountOffers = []) {
-    this.discountOffers = discountOffers;
-  }
-  updateDiscounts() {
-    for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-          }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
-          }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          }
-        }
-      }
+class Partner {
+    constructor(discountOffer) {
+        this.discountOffer = discountOffer;
     }
 
-    return this.discountOffers;
-  }
+    updateDiscount() {
+        throw new Error('Method not implemented');
+    }
+}
+
+class NormalPartner extends Partner {
+    updateDiscount() {
+
+        if (this.discountOffer.discountInPercent > 0) {
+            this.discountOffer.discountInPercent -= this.discountOffer.expiresIn < 0 ? 2 : 1;
+        }
+
+        this.discountOffer.expiresIn -= 1;
+
+        if (this.discountOffer.discountInPercent < 0) {
+            this.discountOffer.discountInPercent = 0;
+        }
+    }
+}
+
+class NaturaliaPartner extends Partner {
+    updateDiscount() {
+        if (this.discountOffer.discountInPercent < 50) {
+            this.discountOffer.discountInPercent += this.discountOffer.expiresIn < 1 ? 2 : 1;
+        }
+
+        this.discountOffer.expiresIn -= 1;
+
+        if (this.discountOffer.discountInPercent > 50) {
+            this.discountOffer.discountInPercent = 50;
+        }
+    }
+}
+
+class IlekPartner extends Partner {
+    updateDiscount() {
+       // No discount update needed for IlekPartner
+    }
+}
+
+class VintedPartner extends Partner {
+    updateDiscount() {
+        
+        if (this.discountOffer.expiresIn > 10) {
+            this.discountOffer.discountInPercent += 1;
+        }
+        else if (this.discountOffer.expiresIn > 5) {
+            this.discountOffer.discountInPercent += 2;
+        }
+        else if (this.discountOffer.expiresIn > 0) {
+            this.discountOffer.discountInPercent += 3;
+        }
+        else {
+            this.discountOffer.discountInPercent = 0;
+        }
+
+        this.discountOffer.expiresIn -= 1;
+
+        if (this.discountOffer.discountInPercent > 50) {
+            this.discountOffer.discountInPercent = 50;
+        }
+        
+    }
+}
+
+class BackMarketPartner extends Partner {
+    updateDiscount() {
+
+        if (this.discountOffer.discountInPercent > 0) {
+            this.discountOffer.discountInPercent -= this.discountOffer.expiresIn < 0 ? 4 : 2;
+        }
+
+        this.discountOffer.expiresIn -= 1;
+
+        if (this.discountOffer.discountInPercent < 0) {
+            this.discountOffer.discountInPercent = 0;
+        }
+    }
+}
+
+
+export class Store {
+    
+    constructor(discountOffers = []) {
+        this.discountOffers = discountOffers;
+    }
+
+    createPartner(discountOffer) {
+        switch (discountOffer.partnerName) {
+            case 'Naturalia':
+                return new NaturaliaPartner(discountOffer);
+            case 'Ilek':
+                return new IlekPartner(discountOffer);
+            case 'Vinted':
+                return new VintedPartner(discountOffer);
+            case 'BackMarket':
+                return new BackMarketPartner(discountOffer);
+            default:
+                return new NormalPartner(discountOffer);
+        }
+    }
+
+    updateDiscounts() {
+        this.discountOffers = this.discountOffers.map((discountOffer) => {
+            const partner = this.createPartner(discountOffer);
+            partner.updateDiscount();
+            return partner.discountOffer;
+        });
+
+        return this.discountOffers;
+    }
+
+    
 }
